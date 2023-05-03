13df2271cef0764889a92cc3370a6f1995707062
Kevin Page <kevin.page1.ext@bayer.com>
2023-05-02 22:52:59 -0500

    Initial commit

---
+++ src/main/java/com/kevinpage/charter/assessment/rewards/RewardsApplication.java
@@ -0,0 +1,14 @@
+package com.kevinpage.charter.assessment.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsApplication.class, args);
+		System.err.println("Well hi there");
+	}
+
+}
